3722d18324a13187677b219ee8cabbfaa5a8e113
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-05-21 23:29:59 +0700

    change app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('blog/index.html')
+    return render_template('main/index.html')
 
 if __name__ == '__main__':
     app.run()
